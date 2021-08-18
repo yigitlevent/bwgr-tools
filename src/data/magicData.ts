7f5801368ddbdd2efe5b47d969626399d93ff12c
yigitlevent <yigitlevent@live.com>
2021-08-18 18:59:50 +0300

    Magic data

---
+++ src/data/magicData.ts
@@ -0,0 +1,45 @@
+export const MagicData: string[][] = [
+	[ // 3 origin
+		"Personal",
+		"Presence",
+		"Sight"
+	],
+	[ // 4 duration
+		"Instantaneous",
+		"Presence",
+		"Permanent",
+		"Sustained"
+	],
+	[ // 7 impetus
+		"Create",
+		"Destroy",
+		"Tax",
+		"Transmute",
+		"Control",
+		"Influence",
+		"Enhance"
+	],
+	[ // 8 elements
+		"Anima",
+		"Arcana",
+		"Heaven",
+		"White",
+		"Fire",
+		"Air",
+		"Earth",
+		"Water"
+	],
+	[ // 11 area of effect
+		"Caster",
+		"Double Area",
+		"Measured Area",
+		"Half Area",
+		"Dbl. Nat. Eff.",
+		"Nat. Effect",
+		"Half Nat. Eff.",
+		"Dbl. Presence",
+		"Presence",
+		"Half Presence",
+		"Single Target"
+	],
+];
