7deedfa69c3bb4304870cd6336e41be8b1414573
yigitlevent <yigitlevent@live.com>
2021-08-19 13:31:29 +0300

    LeftBox subcomponent reordering

--- src/components/app/LeftBox.tsx
+++ src/components/app/LeftBox.tsx
@@ -4,10 +4,10 @@ import { Title } from "../shared/Titles";
 import { LifepathExplorer } from "./leftbox/LifepathExplorer";
 import { SkillExplorer } from "./leftbox/SkillExplorer";
 import { TraitExplorer } from "./leftbox/TraitExplorer";
+import { Faction } from "./leftbox/Faction";
+// import { Unit } from "./leftbox/Unit";
 import { MagicWheel } from "./leftbox/MagicWheel";
 // import { Distillation } from "./leftbox/Distillation";
-import { Faction } from "./leftbox/Faction";
-import { Unit } from "./leftbox/Unit";
 
 export function LeftBox(): JSX.Element {
 
@@ -17,10 +17,10 @@ export function LeftBox(): JSX.Element {
 			<LifepathExplorer />
 			<SkillExplorer />
 			<TraitExplorer />
+			<Faction />
+			{/*<Unit />*/}
 			<MagicWheel />
 			{/*<Distillation />*/}
-			<Faction />
-			<Unit />
 		</Box>
 	);
 }
