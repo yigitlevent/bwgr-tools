52cd51e5a76ec10e7937a61a58ad842178b2a4ec
Yiğit Levent <yigitlevent@live.com>
2022-08-26 22:25:54 +0300

    fixes vite mime issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	base: "/bwgr-tools/",
 	plugins: [react()],
 	resolve: {
 		alias: {
-			'@mui/styled-engine': '@mui/styled-engine-sc',
+			"@mui/styled-engine": "@mui/styled-engine-sc",
 		},
 	},
 });
