e2be88099e30fabb882d7754bf952e1772a8022d
Yiğit Levent <yigitlevent@live.com>
2022-09-10 14:58:19 +0300

    switches to hashrouter because of github pages limitation

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
+import {  HashRouter } from "react-router-dom";
 
 import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider } from "@mui/material/styles";
@@ -18,9 +18,9 @@ ReactDOM
 			<Provider store={STORE}>
 				<ThemeProvider theme={THEME}>
 					<CssBaseline />
-					<BrowserRouter basename="/bwgr-tools">
+					<HashRouter>
 						<App />
-					</BrowserRouter>
+					</HashRouter>
 				</ThemeProvider >
 			</Provider>
 		</StrictMode>
