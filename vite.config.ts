f4c710964472c7617428771d31601e2f23b79259
Yiğit Levent <yigitlevent@live.com>
2022-10-22 15:14:02 +0300

    fixes chunk sizes

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,29 @@
-import { defineConfig } from "vite";
-import { chunkSplitPlugin } from "vite-plugin-chunk-split";
+import { defineConfig, splitVendorChunkPlugin } from "vite";
 import react from "@vitejs/plugin-react";
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	base: "/bwgr-tools/",
 	plugins: [
-		chunkSplitPlugin({
-			strategy: "default",
-			customSplitting: {
-				"react": ["react", "react-dom", "react-redux", "react-router-dom"],
-				"mui": ["@mui/material", "@mui/styled-engine-sc", "@mui/icons-material"],
-				"data.st": [/src\/data\/stocks/],
-				"data.sk": [/src\/data\/skills/],
-				"data.tr": [/src\/data\/traits/],
-				"data": [/src\/data/]
-			}
-		}),
+		splitVendorChunkPlugin(),
 		react()
 	],
 	resolve: {
 		alias: {
 			"@mui/styled-engine": "@mui/styled-engine-sc"
+		}
+	},
+	build: {
+		rollupOptions: {
+			output: {
+				manualChunks: (id) => {
+					if (id.includes("/src/data/stocks")) return "d.st";
+					else if (id.includes("/src/data/skills")) return "d.sk";
+					else if (id.includes("/src/data/traits")) return "d.tr";
+					else if (id.includes("/src/data")) return "d";
+				}
+			}
 		}
 	}
 });
