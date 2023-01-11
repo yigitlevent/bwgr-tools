cc200b0735d92f5c37118124a52a6c8363602187
Yiğit Levent <yigitlevent@live.com>
2023-01-11 06:54:28 +0300

    testing something

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: "/bwgr-tools/",
+	//base: "/bwgr-tools/",
 	plugins: [
 		splitVendorChunkPlugin(),
 		react()
